7acaba4c4bb5ddc55bfc1265ba700294dffaaeb9
gsh7787 <gsh7787@naver.com>
2023-04-11 16:56:21 +0900

    글쓰기 restful api 구현

--- frontend/src/pages/board/Write.js
+++ frontend/src/pages/board/Write.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Button, Form} from "react-bootstrap";
+import axios from "axios";
+import {useNavigate} from "react-router-dom"
+
 
 const Write = () => {
+
+    const navigate  = useNavigate();
+
+    const [title,setTitle] = useState();
+    const [contents,setContents] = useState();
+    const [writer,setWriter] = useState();
+
+    const submitBtn = () => {
+
+        let data = {
+            title: title,
+            contents: contents,
+            writer: writer
+        }
+
+        axios.post("/api/board/write", JSON.stringify(data), {
+            headers: {
+                "Content-Type": 'application/json',
+            },
+        })
+        .then((res) => {
+            if (res !== null) {
+                alert("성공");
+                navigate("/board/list");
+            } else {
+                alert("에러");
+            }
+        })
+    }
+
     return (
         <div>
-            <h1>글쓰기</h1>
+            <div>
+                <input type="text" name='title' value={title} onChange={(e)=>setTitle(e.target.value)}/>
+            </div>
+            <div>
+                <input type="text" name='title' value={writer} onChange={(e)=>setWriter(e.target.value)}/>
+            </div>
+            <div>
+                <textarea name='content' value={contents} onChange={(e)=>setContents(e.target.value)}/>
+            </div>
+            <button onClick={submitBtn}>글 작성</button>
         </div>
     );
 };
--- src/main/java/com/sihun/jpastudy/controller/ApiController.java
+++ src/main/java/com/sihun/jpastudy/controller/ApiController.java
@@ -1,24 +1,29 @@
 package com.sihun.jpastudy.controller;
 
+import com.sihun.jpastudy.dto.BoardDto;
 import com.sihun.jpastudy.entity.BoardEntity;
 import com.sihun.jpastudy.service.BoardService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/")
 public class ApiController {
 
     private final BoardService boardService;
 
 
-    @GetMapping("/api/board/list")
+    @GetMapping("board/list")
     public List<BoardEntity> findAll() throws Exception {
         return boardService.findAll();
+    }
 
+    @PostMapping("board/write")
+    public void write(@RequestBody BoardDto boardDto) throws Exception{
+        boardService.addBoard(boardDto);
     }
 }
